b9c91c6c3151fc05fe103055e0f2caaf0dbc3ca9
Derrick Eckardt <derrick.eckardt@gmail.com>
2020-05-19 21:02:35 +0000

    working on envato functionality

--- envato.py
+++ envato.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 import requests
 
+request_url = "https://api.envato.com/v1/discovery/search/search/item"
+headers = {
+    "Authorization" : "Bearer oVi4yPxk1bJ64Y2qOsLJ2D2ZlC3FpK4L",
+    }
+
+post = {
+    "site" : "themeforest.net",
+    "term" : "awesome"
+}    
+
+def item_search(request_url, headers, post):
+    r = requests.post(request_url, headers=headers, json=post)
+    return r
+
+item = item_search(request_url, headers, post)
+
+print(item)
